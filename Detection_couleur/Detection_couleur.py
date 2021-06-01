29c9c3d19342ad5c7d17cc8681bee12fe1a392a6
Emma5099 <emma.genthon@cpe.fr>
2021-06-01 16:46:37 +0200

    maj2

--- Detection_couleur/Detection_couleur.py
+++ Detection_couleur/Detection_couleur.py
@@ -4,20 +4,34 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import math
-from cv2 import cv2
-from PIL import Image
+import cv2
+from PIL.Image import * 
 
-
+Chemin="Images_Resistance/resistance_9.png"
 #récupération des bandes en noir
-colorImage = cv2.imread("Images_Resistance/resistance_9.png")
-grayImage = cv2.imread("Images_Resistance/resistance_9.png",0)
-ret,tresh=cv2.threshold(grayImage,190,255,cv2.THRESH_BINARY_INV)
+colorImage = cv2.imread(Chemin)
+grayImage = cv2.imread(Chemin,0)
+ret,tresh=cv2.threshold(grayImage,193,255,cv2.THRESH_BINARY_INV)
 masked=cv2.bitwise_and(colorImage, colorImage,mask=tresh)
 
-cv2.imshow('monImage',tresh)
-cv2.waitKey(0)
+ker1=cv2.getStructuringElement(cv2.MORPH_RECT,(18,11))
+ouverture1=cv2.morphologyEx(masked, cv2.MORPH_OPEN, ker1) # ouverture de I par S
 
-cv2.imshow('monImage',masked)
-cv2.waitKey(0)
-cv2.imshow('monImage',colorImage)
-cv2.waitKey(0)
+ker2=cv2.getStructuringElement(cv2.MORPH_RECT,(10,10))
+ouverture2=cv2.morphologyEx(ouverture1, cv2.MORPH_OPEN, ker2) # ouverture de I par S
+
+
+plt.axis('off')
+plt.imshow(cv2.cvtColor(ouverture2, cv2.COLOR_BGR2RGB))
+plt.show()
+Recup_Couleur=open(Chemin)
+
+premiere_bande=Image.getpixel(Recup_Couleur,(40,70))
+print(premiere_bande)
+deuxieme_bande=Image.getpixel(Recup_Couleur,(40,90))
+print(deuxieme_bande)
+troiseme_bande=Image.getpixel(Recup_Couleur,(40,120))
+print(troiseme_bande)
+
+
+
