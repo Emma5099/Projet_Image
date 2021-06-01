cf34cbd07faf2f6199b1b7030d2e40109f1a0326
Emma5099 <emma.genthon@cpe.fr>
2021-06-01 14:52:10 +0200

    maj

--- Detection_couleur/Detection_couleur.py
+++ Detection_couleur/Detection_couleur.py
@@ -1,11 +1,23 @@
-#Importations des librairies
-import numpy as np 
-import cv2 
-import matplotlib.pyplot as plt 
-from PIL.Image import * 
+###Code###
+#Ce code met en oeuvre la methode de segmentation du snake
+##########
+import numpy as np
+from matplotlib import pyplot as plt
+import math
+from cv2 import cv2
+from PIL import Image
 
-#Lecture de l'image 
-Chemin = "Images_Resistance/resistance_1.png"
 
-Image_Couleur = open(Chemin)
-Image_Forme = cv2.imread(Chemin) 
+#récupération des bandes en noir
+colorImage = cv2.imread("Images_Resistance/resistance_9.png")
+grayImage = cv2.imread("Images_Resistance/resistance_9.png",0)
+ret,tresh=cv2.threshold(grayImage,190,255,cv2.THRESH_BINARY_INV)
+masked=cv2.bitwise_and(colorImage, colorImage,mask=tresh)
+
+cv2.imshow('monImage',tresh)
+cv2.waitKey(0)
+
+cv2.imshow('monImage',masked)
+cv2.waitKey(0)
+cv2.imshow('monImage',colorImage)
+cv2.waitKey(0)
